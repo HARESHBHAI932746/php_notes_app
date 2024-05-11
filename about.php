d4f25a4c45d1424bb703856ebcf966968b73d425
HARESHBHAI932746 <165651038+HARESHBHAI932746@users.noreply.github.com>
2024-05-11 11:22:43 +0530

    Update about.php

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>about this website</h1>
+    <p>
+      Welcome to our Notes WebApp.Register or log in to access your parsonalized
+      note-taking experience.once Logged in,you can effortlessly create,edit and
+      delete your notes
+    </p>
+  </body>
+</html>
